4c2d6ac418a9368fbecdab13c6fd1d97bd9b6d06
Jinyoung Jang <jyjang@uengine.org>
2022-12-13 13:37:35 +0900

    commit

---
+++ common-api/src/main/java/human/resource/mgmt/command/RegisterUserCommand.java
@@ -0,0 +1,15 @@
+package human.resource.mgmt.command;
+
+import human.resource.mgmt.query.*;
+import java.util.List;
+import lombok.Data;
+import lombok.ToString;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+@ToString
+@Data
+public class RegisterUserCommand {
+
+    private String userId; // Please comment here if you want user to enter the id directly
+    private Integer dayCount;
+}
